931803af2a96ea6f30582af46c2f06722f6abb7d
Maria Leitão <malufln.99@gmail.com>
2024-05-17 13:57:41 -0300

    Initializing Rest server with Go

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Home Page")
+}
+
+func HandleRequest() {
+	http.HandleFunc("/", Home)
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
+
+func main() {
+	fmt.Println("Initializing Rest server with Go")
+	HandleRequest()
+}
