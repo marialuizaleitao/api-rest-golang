13c7fa76318776cb5631f35df9f0d4b813b256ae
Maria Leitão <malufln.99@gmail.com>
2024-05-17 15:54:36 -0300

    Add Docker Compose configuration and initial SQL script for database setup

---
+++ migration/docker-database-initial.sql
@@ -0,0 +1,12 @@
+CREATE TABLE pilots
+(
+    id      serial primary key,
+    name    varchar,
+    history varchar
+);
+
+INSERT INTO pilots(name, history)
+VALUES ("Ayrton Senna",
+        "Ayrton Senna was a Brazilian racing driver who won three Formula One world championships for McLaren in 1988, 1990, and 1991. He is considered one of the greatest Formula 1 drivers of all time due to his exceptional speed, skill, and intense competitive spirit. Senna's tragic death at the 1994 San Marino Grand Prix shocked the world and left an indelible mark on the sport."),
+       ("Emerson Fittipaldi",
+        "Emerson Fittipaldi is a Brazilian racing driver who became the youngest Formula One world champion at the time when he won the title in 1972 at the age of 25. He won his second world championship in 1974. Fittipaldi is also known for his success in other racing series, including CART and IndyCar. He remains one of Brazil\'s most successful racing drivers.");
